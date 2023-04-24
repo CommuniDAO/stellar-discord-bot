ea43208add338c5f732aee3fe75546ae2f54e47f
Julian Clatro <jclatro@yahoo.com>
2023-04-24 12:34:48 -0300

    redirect to index if session is not set at connect

--- app/routes/connect.tsx
+++ app/routes/connect.tsx
@@ -1,4 +1,16 @@
 import { ImportAccount } from "~/templates/ImportAccount";
+import { type LoaderArgs, redirect } from "@remix-run/cloudflare";
+import { getUser } from "~/utils/session.server";
+
+export const loader = async ({ request, context }: LoaderArgs) => {
+  const { DB, sessionStorage } = context as any;
+
+  const user =  await getUser(request, sessionStorage)
+  if (!user) return redirect('/')
+  
+  return null
+};
+
 
 export default function Index() {
   return (
